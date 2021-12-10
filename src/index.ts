612e430f87686e4b4d5b10f757d1c3d81b9d8700
heppokofrontend <heppokofrontend@gmail.com>
2021-12-11 00:13:07 +0900

    :recycle: refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 import path from 'path';
 import fs from 'fs/promises';
-import macos from './modules/macos';
-import windows from './modules/windows';
-import windowsGoogle from './modules/windows--google';
+import {format} from './utils/format';
 
-const write = async (cb: DFB.WriteFunction, src: DFB.IME_Dictionary[], fp: string): Promise<string> => {
+/** TSV or CSV */
+const sep = {
+  win: '\t',
+  mac: ',',
+}
+/**
+ * 書き出し
+ * @param dict - 実際に書き出すテキストデータ
+ * @param fp - 書き出し先のファイルパス
+ */
+const write = async (dict: string, fp: string) => {
   const {root} = path.parse(fp);
   const target = root === '' ? path.join(process.cwd(), fp) : fp;
 
@@ -13,34 +21,47 @@ const write = async (cb: DFB.WriteFunction, src: DFB.IME_Dictionary[], fp: strin
     recursive: true,
   });
 
-  return cb(src, target);
+  await fs.writeFile(
+    target,
+    Buffer.from(`\ufeff${dict}`, 'utf16le'),
+  );
 };
 
-module.exports = (src: DFB.IME_Dictionary[], type: string, dist?: string) => {
+/**
+ * String.prototype.replaceの第２引数
+ * @param ｓ - ひらがな１文字
+ * @returns - カタカナ１文字
+ */
+const replacer = (s: string) => String.fromCharCode(s.charCodeAt(0) + 0x60);
+
+/**
+ * Build-dictionary-files
+ * @param src - 辞書データオブジェクト
+ * @param type - 書き出すファイルのフォーマット
+ * @param dist - 書き出し先のファイルパス
+ * @returns　実際に書き出すテキストデータ
+ */
+module.exports = async (
+  src: DFB.IME_Dictionary[],
+  type: 'win' | 'win-google' | 'mac',
+  dist?: string
+) => {
   const {dir, base, ext} = path.parse(dist || process.cwd());
-
-  switch (type) {
-    case 'win': {
-      const filename = ext === '.txt' ? base : path.join(base, './dict--win.txt');
-      const filePath = path.join(dir, filename);
-
-      return write(windows, src, filePath);
+  const filename = ext === '.txt' ? base : path.join(base, `./dict--${type}.txt`);
+  const filePath = path.join(dir, filename);
+  const platform = type.startsWith('win') ? 'win' : 'mac';
+  const data = src.map(item => {
+    // GoogleIME用に読み仮名をカタカナに変換する
+    if (type === 'win-google') {
+      item.input = item.input?.replace(/[ぁ-ん]/g, replacer);
     }
 
-    case 'winGoogle': {
-      const filename = ext === '.txt' ? base : path.join(base, './dict--win-google.txt');
-      const filePath = path.join(dir, filename);
+    const list: DFB.Format = format(item, platform);
 
-      return write(windowsGoogle, src, filePath);
-    }
+    return list.join(sep[platform]);
+  }).join(type === 'win-google' ? '\r\n' : '\n');
 
-    case 'mac': {
-      const filename = ext === '.txt' ? base : path.join(base, './dict--macos.txt');
-      const filePath = path.join(dir, filename);
+  await write(data, filePath);
 
-      return write(macos, src, filePath);
-    }
-  }
-
-  return new Promise<string>(r => r(''));
+  return data;
 }
