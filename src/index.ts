b613b10e6288ef8766d003efa18211eef6b75894
heppokofrontend <heppokofrontend@gmail.com>
2021-12-10 16:32:02 +0900

    :recycle: Update

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,40 @@ import macos from './modules/macos';
 import windows from './modules/windows';
 import windowsGoogle from './modules/windows--google';
 
+const write = async (cb: DFB.WriteFunction, src: DFB.IME_Dictionary[], fp: string): Promise<string> => {
+  const {root} = path.parse(fp);
+  const target = root === '' ? path.join(process.cwd(), fp) : fp;
+
+  await fs.mkdir(path.parse(target).dir, {
+    recursive: true,
+  });
+
+  return cb(src, target);
+};
+
 module.exports = (src: DFB.IME_Dictionary[], type: string, dist?: string) => {
   const {dir, base, ext} = path.parse(dist || process.cwd());
-  const write = async (cb: DFB.WriteFunction, fp: string): Promise<string> => {
-    const {root} = path.parse(fp);
-    const target = root === '' ? path.join(process.cwd(), fp) : fp;
-
-    await fs.mkdir(path.parse(target).dir, {
-      recursive: true,
-    });
-
-    return cb(src, target);
-  };
 
   switch (type) {
     case 'win': {
       const filename = ext === '.txt' ? base : path.join(base, './dict--win.txt');
       const filePath = path.join(dir, filename);
 
-      return write(windows, filePath);
+      return write(windows,, src, filePath);
     }
 
     case 'winGoogle': {
       const filename = ext === '.txt' ? base : path.join(base, './dict--win-google.txt');
       const filePath = path.join(dir, filename);
 
-      return write(windowsGoogle, filePath);
+      return write(windowsGoogle,, src, filePath);
     }
 
     case 'mac': {
       const filename = ext === '.txt' ? base : path.join(base, './dict--macos.txt');
       const filePath = path.join(dir, filename);
 
-      return write(macos, filePath);
+      return write(macos,, src, filePath);
     }
   }
 
