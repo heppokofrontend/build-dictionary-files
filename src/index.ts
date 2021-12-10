c022b3dfa98d5502c3b14bb97087ba487f78ccd3
heppokofrontend <heppokofrontend@gmail.com>
2021-12-10 14:57:17 +0900

    :zap: Add help of `dfb`

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,16 @@ import windows from './modules/windows';
 import windowsGoogle from './modules/windows--google';
 
 module.exports = async (dist: string, src?: DFB.IME_Dictionary[]) => {
-  const argv = await yargs(hideBin(process.argv)).argv;
+  const argv = await yargs(hideBin(process.argv)).option('win', {
+    type: 'boolean',
+    description: 'Windows10標準IME向け辞書データの書き出し',
+  }).option('winGoogle', {
+    type: 'boolean',
+    description: 'Windows10 GoogleIME向け辞書データの書き出し',
+  }).option('mac', {
+    type: 'boolean',
+    description: 'macOS向け辞書データの書き出し',
+  }).argv;
   const data = src || (
     JSON.parse(
       typeof argv._[0] === 'string' && await fs.readFile(argv._[0], {
